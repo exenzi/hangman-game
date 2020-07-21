57671f206dfc9d6dad64f963330b9aef4b278a91
Max Rokitsky <exenzi@gmail.com>
2020-07-22 00:48:39 +0300

    add comments

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import random
 from hangman import game_ui, game_logic
 
+# Нужно ли выводить загаданное слово
 DEBUG = True
 
 random_line = random.choice(open("words.txt").readlines()).rstrip('\n')
+
+# Создаём игру и передаём в виде параметра классу UI
 game = game_logic.Logic(random_line)
 ui = game_ui.UI(game)
 
